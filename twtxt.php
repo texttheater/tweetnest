2b2d8d0f74ea26e86c247a4223b9998355d31c72
Kilian Evang <noreply@texttheater.net>
2016-02-07 18:34:44 +0100

    Now displaying RTs fully

--- twtxt.php
+++ twtxt.php
@@ -8,8 +8,21 @@
 	$q = $db->query("SELECT `".DTP."tweets`.*, `".DTP."tweetusers`.`screenname`, `".DTP."tweetusers`.`realname`, `".DTP."tweetusers`.`profileimage` FROM `".DTP."tweets` LEFT JOIN `".DTP."tweetusers` ON `".DTP."tweets`.`userid` = `".DTP."tweetusers`.`userid` ORDER BY `".DTP."tweets`.`time` DESC");
 
 	while($tweet = $db->fetch($q)){
+		$tweetextra = array();
+
+		if(!empty($tweet['extra'])){
+			@$tweetextra = unserialize($tweet['extra']);
+		}
+
+		$rt = (is_array($tweetextra) && array_key_exists("rt", $tweetextra) && !empty($tweetextra['rt']));
+
+		if($rt) {
+			$retweet = $tweetextra['rt'];
+		}
+
 		$date = date(DATE_ISO8601, $tweet['time']);
-		$text = str_replace("\n", ' ', $tweet['text']);
+		$text = $rt ? 'RT @' . $retweet['screenname'] . ': ' . $retweet['text'] : $tweet['text'];
+		$text = str_replace("\n", ' ', $text);
 		printf("%s\t%s\n", $date, $text);
 	}
 ?>
